56862c2a6e15c2c52358643fb2ac9d04519f5518
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-12 17:19:01 +0530

    signup pge done

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Loading from "./Loading";
+import Logo from "./Logo";
+import Button from "./Button";
+import Divider from "./Divider";
+import Inputbox from "./Inputbox";
+export {
+    Button, Loading,Logo, Divider,Inputbox
+};
