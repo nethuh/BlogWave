83f373a4cb637e66fa75f97ff3e7d54a463481b8
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-12 19:06:53 +0530

    home page designing started

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import { posts } from "../utils/dummyData";
 
 const Home = () => {
-  const posts = posts
+  const numOfPages = 4;
+  const [page, setPage] = useState(0);
+
+  const randomIndex = Math.floor(Math.random() * posts.length);
   return <div>Home</div>;
 };
 
