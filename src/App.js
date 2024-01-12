5ce783867f16e5f06ee73ae689bd2a7cabd1c9ae
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-12 17:17:43 +0530

    src files addd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {Routes, Route, Outlet} from "react-router-dom"
+import {BlogDetails, CategoriesPage, Home, WriterPage, SignupPage, LoginPage} from "./pages";
+import {Loading} from "./components"
+function Layout() {
+    return (
+        <div className="w-full flex flex-col
+        min-h-screen px-4 md:px-10 2xl:px-29=8">
+            {/*<Navbar/>*/}
+
+            <div className='flex-1'>
+                <Outlet/>
+            </div>
+            {/*<Footer/>*/}
+        </div>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const theme = "dark";
+    const isLoading = false;
+    return (
+        <main className={theme}>
+            <div className="w-full min-h-screen
+      relative bg-white dark:bg-[#020b19]">
+                <Routes>
+                    <Route element={<Layout/>}>
+                        <Route path='/' element={<Home/>}/>
+                        <Route path='/category' element={<CategoriesPage/>}/>
+                        <Route path='/:slug/:id?' element={<BlogDetails/>}/>
+                        <Route path='/writer/:id' element={<WriterPage/>}/>
+                    </Route>
+
+                    <Route path='/signup' element={<SignupPage/>}/>
+                    <Route path='/signin' element={<LoginPage/>}/>
+
+                </Routes>
+
+                {isLoading && <Loading/>}
+            </div>
+        </main>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from "react-router-dom";
+import {GoogleOAuthProvider} from "@react-oauth/google"
 
+const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <GoogleOAuthProvider clientId={clientId}>
+                <App/>
+            </GoogleOAuthProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
