f1782ff7297c1a2b729b1ecf2a0592318ed9f74e
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-13 17:52:50 +0530

    updated

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -1,4 +1,11 @@
 const Pagination = ({ totalPages, onPageChange }) => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const currentPage = parseInt(searchParams.get("page")) || 1;
+
+    const range = (start, end) =>
+        Array.from({ length: end - start + 1}, (_, i) => start + i);
+
+    const showEllipses = totalPages > 8;
     return(
         <div>
 
