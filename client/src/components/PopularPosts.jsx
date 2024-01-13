cb8865dde11b34dc0334f736d06f7333ae2754ca
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-13 19:15:44 +0530

    popular posts updated

--- client/src/components/PopularPosts.jsx
+++ client/src/components/PopularPosts.jsx
@@ -3,6 +3,15 @@ import { Link } from "react-router-dom";
 import { CATEGORIES } from "../utils/dummyData";
 
 const PopularPosts = ({ posts }) => {
+    const Card = ({ post }) => {
+        let catColor = "";
+        CATEGORIES.map((cat) => {
+            if (cat.label === post?.cat){
+                catColor = cat?.color;
+            }
+            return null;
+        })
+    }
     return(
         <div>
 
