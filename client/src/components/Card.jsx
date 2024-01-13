50043dab4b136e972b376cb779479c7024738466
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-13 17:19:55 +0530

    card updated

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -5,7 +5,18 @@ import { Link } from "react-router-dom";
 
 const Card = ({ post, index }) => {
     return (
-        <div></div>
+        <div className='w-full flex flex-col gap-8 items-center rounded md:flex-row'>
+
+            <Link to={`/${post?.slug}/${post._id}`}
+            className='w-full h-auto md:h-64 md:w-2/4'
+            >
+                <img
+                src={post?.img}
+                alt={post?.title}
+                className='object-cover w-full h-full rounded'
+                />
+            </Link>
+        </div>
     );
 };
 export default Card;
