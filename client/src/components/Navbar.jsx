aa96ced39ee0589759ad1c50709b51d3f0935614
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-14 15:10:48 +0530

    updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,6 +12,18 @@ import Button from "./Button";
 import Logo from "./Logo";
 import ThemeSwitch from "./Switch";
 
+function getInitials(fullName) {
+    const names = fullName.split(" ");
+
+    const initials = names.slice(0, 2).map((name) => name[0].toUpperCase());
+
+    const initialsStr = initials.join("");
+
+    return initialsStr;
+}
+
+
+
 const Navbar = () => {
     return (
         <div></div>
