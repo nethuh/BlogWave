5bb63293e00257f78323e5366499912a8d0f62a6
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-16 00:19:04 +0530

    CategoriesPage updated

--- client/src/pages/CategoriesPage.jsx
+++ client/src/pages/CategoriesPage.jsx
@@ -19,6 +19,33 @@ const CategoriesPage = () => {
             {query}
           </h2>
         </div>
+
+          <div className='w-full flex flex-col md:flex-row gap-10 2xl:gap-20'>
+              {/* LEFT */}
+              <div className='w-full md:w-2/3 flex flex-col gap-y-28 md:gap-y-14'>
+                  {posts?.length === 0 ? (
+                      <div className='w-full h-full py-8 flex justify-center'>
+                          <span className='text-lg text-slate-500'>
+                No Post Available for this category
+              </span>
+                      </div>
+                  ) : (
+                      <>
+                          {posts?.map((post) => (
+                              <Card key={post?._id} post={post} />
+                          ))}
+
+                          <div className='w-full flex items-center justify-center'>
+                              <Pagination
+                                  totalPages={numOfPages}
+                                  onPageChange={handlePageChange}/>
+                          </div>
+                      </>
+                  )}
+              </div>
+
+              {/* RIGHT */}
+          </div>
       </div>
   );
 };
