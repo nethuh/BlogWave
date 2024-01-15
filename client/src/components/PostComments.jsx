d15c5e55436a813ed454325a608c419afad1d9f3
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-16 01:01:18 +0530

    updated

--- client/src/components/PostComments.jsx
+++ client/src/components/PostComments.jsx
@@ -14,7 +14,28 @@ const PostComments = ({postId}) => {
     const handleDeleteComment = async (id) => {};
 
     return (
-        <div></div>
+        <div className='w-full py-10'>
+            <p className='text-lg text-slate-700 dark:text-slate-500 mb-6'>
+                Post Comments
+            </p>
+
+            {user?.token ? (
+                <form className='flex flex-col mb-6'>
+                    <textarea
+                        name='desc'
+                        onChange={(e) => setDesc(e.target.value)}
+                        value={desc}
+                        required={true}
+                        placeholder='Add a comment...'
+                        className='bg-transparent w-full p-2 border border-gray-300 focus:outline-none focus:border-blue-600  focus:ring-blue-600 rounded'
+                    ></textarea>
+
+                    <div className='w-full flex justify-end mt-2'>
+
+                    </div>
+                </form>
+            )}
+        </div>
     )
 };
 
