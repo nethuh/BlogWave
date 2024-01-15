66eea61757960e21dd3f965659832dcd880ae017
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-15 23:10:26 +0530

    updated

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -130,7 +130,22 @@ const Navbar = () => {
 
     console.log(user);
     return (
-        <div></div>
+        <nav className='flex flex-col md:flex-row w-full py-5 items-center justify-between gap-4 md:gap-0'>
+            <div className='flex gap-2 text-[20px] md:hidden lg:flex'>
+                <Link to='/' className='text-red-600'>
+                    <FaYoutube/>
+                </Link>
+                <Link to='/' className='text-blue-600'>
+                    <FaFacebook/>
+                </Link>
+                <Link to='/' className='text-rose-600'>
+                    <FaInstagram/>
+                </Link>
+                <Link to='/' className='text-blue-500'>
+                    <FaTwitterSquare/>
+                </Link>
+            </div>
+        </nav>
     );
 };
 export default Navbar;
