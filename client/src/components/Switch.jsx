f4bfaf79a1f26c20637f69dce21c5388e1253327
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-15 23:48:19 +0530

    updated

--- client/src/components/Switch.jsx
+++ client/src/components/Switch.jsx
@@ -2,6 +2,15 @@ import React, { useState } from "react";
 import useStore from "../store";
 
 const ThemeSwitch = () => {
+    const { theme , setTheme } = useStore();
+    const [isDarkMode, setIsDarkMode] = useState(theme === "dark");
+
+    const toggleTheme = () => {
+        const newTheme = isDarkMode ? "light" : "dark";
+        setIsDarkMode(!isDarkMode);
+        setTheme(newTheme);
+        localStorage.setItem("theme", newTheme);
+    };
     return (
         <div></div>
     );
