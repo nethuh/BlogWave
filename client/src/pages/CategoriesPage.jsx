146886a1d9034772c2427463298d9297666e43c5
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-16 00:01:07 +0530

    CategoriesPage added

--- client/src/pages/CategoriesPage.jsx
+++ client/src/pages/CategoriesPage.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { Card, Pagination, PopularPosts, PopularWriters } from "../components";
+import { popular, posts } from "../utils/dummyData";
+
 const CategoriesPage = () => {
   return <div>CategoriesPage</div>;
 };
