92cad57d1aa7bc4a2cd746245eb26bd1667846cb
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-16 01:13:54 +0530

    writerPage added

--- client/src/pages/WriterPage.jsx
+++ client/src/pages/WriterPage.jsx
@@ -1,3 +1,18 @@
+import { useState } from "react";
+import { FaUserCheck } from "react-icons/fa";
+import { useParams } from "react-router-dom";
+import NoProfile from "../assets/profile.png";
+import {
+  Button,
+  Card,
+  Pagination,
+  PopularPosts,
+  PopularWriters,
+} from "../components";
+import useStore from "../store";
+import { formatNumber } from "../utils";
+import { popular, posts, writer } from "../utils/dummyData";
+
 const WriterPage = () => {
   return <div>WriterPage</div>;
 };
