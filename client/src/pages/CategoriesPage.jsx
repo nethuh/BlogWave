002a48dd84eb119e5ff0a63bb8427813d409f315
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-16 00:07:39 +0530

    updated

--- client/src/pages/CategoriesPage.jsx
+++ client/src/pages/CategoriesPage.jsx
@@ -3,7 +3,18 @@ import { Card, Pagination, PopularPosts, PopularWriters } from "../components";
 import { popular, posts } from "../utils/dummyData";
 
 const CategoriesPage = () => {
-  return <div>CategoriesPage</div>;
+  const query = new URLSearchParams(window.location.search).get("cat");
+  const numOfPages = 4;
+  const [page, setPage] = useState(0);
+
+  const handlePageChange = (val) => {
+    setPage(val);
+
+    console.log(val);
+  }
+  return (
+      <div>CategoriesPage</div>
+  );
 };
 
 export default CategoriesPage;
