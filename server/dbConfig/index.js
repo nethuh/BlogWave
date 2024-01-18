5c5d9594e80c95d52a9f39229aa808ccb81d7328
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-18 18:21:09 +0530

    dbConnected

--- server/dbConfig/index.js
+++ server/dbConfig/index.js
@@ -3,9 +3,7 @@ import mongoose from "mongoose";
 const dbConnection = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URL);
-
         console.log("DB Connected");
-
     }catch (error) {
         console.log("DB Error: " + error);
     }
--- server/index.js
+++ server/index.js
@@ -6,12 +6,21 @@ import helmet from "helmet";
 import morgan from "morgan";
 import dbConnection from "./dbConfig/index.js";
 
-import errorMiddleware from "./middleware/errorMiddleware.js";
-import routes from "./routes/index.js";
+// import errorMiddleware from "./middleware/errorMiddleware.js";
+// import routes from "./routes/index.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8800;
 
-dbConnection()
+dbConnection();
+
+app.use(helmet());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
+
+app.use(morgan("dev"));
