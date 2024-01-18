1f6d8b7261732f2d546a2bdfb83b0fc211c5226e
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-18 18:06:33 +0530

    dbConnection added

--- server/dbConfig/index.js
+++ server/dbConfig/index.js
@@ -1,1 +1,14 @@
 import mongoose from "mongoose";
+
+const dbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+
+        console.log("DB Connected");
+
+    }catch (error) {
+        console.log("DB Error: " + error);
+    }
+};
+
+export default dbConnection;
