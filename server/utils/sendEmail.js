43de73620c218508350e20f6d7f38ade11fa1522
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-21 17:49:47 +0530

    updated

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -3,13 +3,32 @@ import Users from "../models/userModel.js";
 
 export const register = async (req, res, next) => {
     try {
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            image,
+            accountType,
+            provider,
+        } = req.body;
+
+
     }catch (error) {
         console.log(error);
         res.status(404).json({ message: error.message });
     }
 };
 
-export const register = async (req, res, next) => {
+export const googleSignUp = async (req, res, next) => {
+    try {
+    }catch (error) {
+        console.log(error);
+        res.status(404).json({ message: error.message });
+    }
+};
+
+export const login = async (req, res, next) => {
     try {
     }catch (error) {
         console.log(error);
