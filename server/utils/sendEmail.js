0972b00eda83486776433a39660e41169e1d0ebf
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-21 18:15:50 +0530

    updated

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -13,7 +13,34 @@ export const register = async (req, res, next) => {
             provider,
         } = req.body;
 
+        //validate fileds
+        if (!(firstName || lastName || email || password)) {
+            return next("Provide Required Fields!");
+        }
 
+        if (accountType === "Writer" && !image)
+            return next("Please provide profile picture");
+
+        const userExist = await Users.findOne({ email });
+
+        if (userExist) {
+            return next("Email Address already exists. Try Login");
+        }
+
+        const hashedPassword = await hashString(password);
+
+        const user = await Users.create({
+            name: firstName + " " + lastName,
+            email,
+            password: !provider ? hashedPassword : "",
+            image,
+            accountType,
+            provider,
+        });
+
+        user.password = undefined;
+
+        const token = createJWT(user?._id);
     }catch (error) {
         console.log(error);
         res.status(404).json({ message: error.message });
