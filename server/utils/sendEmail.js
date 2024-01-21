edcecde9fcfb91676544e92880c4ac2d597ba41b
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-21 17:41:46 +0530

    controllers Added

---
+++ server/utils/sendEmail.js
@@ -0,0 +1,19 @@
+import Users from "../models/userModel.js";
+
+
+export const register = async (req, res, next) => {
+    try {
+    }catch (error) {
+        console.log(error);
+        res.status(404).json({ message: error.message });
+    }
+};
+
+export const register = async (req, res, next) => {
+    try {
+    }catch (error) {
+        console.log(error);
+        res.status(404).json({ message: error.message });
+    }
+};
+
