d3a9fa0d6f5dbc05ab82f8bbeffc094119a14864
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-21 16:08:04 +0530

    model updated

---
+++ server/models/userModel.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true},
+        emailVerified: { type: Boolean, default: false},
+        accountType: { type: String, default: "User" },
+        image: {type: String},
+        password: {type: String, select: true },
+        provider: {type: String, default: "Nethmi"},
+        followers: [{ type: Schema.Types.ObjectId, ref: "Followers" }],
+
+    }
+)
