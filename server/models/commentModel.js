265d34dc5a2ba6a8017ef24070077af481e1d414
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-21 16:47:51 +0530

    emailVerification updated

--- server/models/commentModel.js
+++ server/models/commentModel.js
@@ -2,7 +2,6 @@ import mongoose, { Schema } from "mongoose";
 
 const commentSchema = new Schema({
     user: {type: Schema.Types.ObjectId, ref: "Users"},
-
     post: {type: Schema.Types.ObjectId, ref: "Posts"},
     desc: {type: String},
 },
