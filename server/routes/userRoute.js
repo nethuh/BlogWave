28ce7b2d3b4b5d272ecf40b24f8dad49ba1e3ce2
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-21 17:33:56 +0530

    routs Added

---
+++ server/routes/userRoute.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+import {
+    googleSignUp,
+    login,
+    register,
+} from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/register", register);
+router.post("/google-signup", googleSignUp);
+router.post("/login", login);
+
+export default router;
