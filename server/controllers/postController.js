4a960f63e39ed03cbc49245b6ce2b665ed8f2a45
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-23 00:58:30 +0530

    getPostContent done

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -8,104 +8,104 @@ import Post from "../models/postModel.js";
 
 
 export const stats = async (req, res, next) => {
-try {
-    const { query } = req.query;
-    const { userId } = req.body.user;
+    try {
+        const {query} = req.query;
+        const {userId} = req.body.user;
 
-    const numofDays = Number(query) || 28;
+        const numofDays = Number(query) || 28;
 
-    const currentDate = new Date();
-    const startDate = new Date();
-    startDate.setDate(currentDate.getDate() - numofDays);
+        const currentDate = new Date();
+        const startDate = new Date();
+        startDate.setDate(currentDate.getDate() - numofDays);
 
-    const totalPosts = await Post.find({
-        user: userId,
-        createdAt: { $gte: startDate, $lte: currentDate },
-    }).countDocuments();
+        const totalPosts = await Post.find({
+            user: userId,
+            createdAt: {$gte: startDate, $lte: currentDate},
+        }).countDocuments();
 
-    const totalViews = await Views.find({
-        user: userId,
-        createdAt: { $gte: startDate, $lte: currentDate },
-    }).countDocuments();
+        const totalViews = await Views.find({
+            user: userId,
+            createdAt: {$gte: startDate, $lte: currentDate},
+        }).countDocuments();
 
-    const totalWriters = await Users.find({
-        accountType: "Writer",
-    }).countDocuments();
+        const totalWriters = await Users.find({
+            accountType: "Writer",
+        }).countDocuments();
 
-    const totalFollowers = await Users.findById(userId);
+        const totalFollowers = await Users.findById(userId);
 
-    const viewStats = await Views.aggregate([
-        {
-            $match: {
-                user: new mongoose.Types.ObjectId(userId),
-                createdAt: { $gte: startDate, $lte: currentDate },
-            },
-        },
-        {
-            $group: {
-                _id: {
-                    $dateToString: { format: "%Y-%m-%d", date: "$createdAt" },
+        const viewStats = await Views.aggregate([
+            {
+                $match: {
+                    user: new mongoose.Types.ObjectId(userId),
+                    createdAt: {$gte: startDate, $lte: currentDate},
                 },
-                Total: { $sum: 1 },
             },
-        },
-        { $sort: { _id: 1 } },
-    ]);
-
-    const followersStats = await Followers.aggregate([
-        {
-            $match: {
-                writerId: new mongoose.Types.ObjectId(userId),
-                createdAt: { $gte: startDate, $lte: currentDate },
-            },
-        },
-        {
-            $group: {
-                _id: {
-                    $dateToString: { format: "%Y-%m-%d", date: "$createdAt" },
+            {
+                $group: {
+                    _id: {
+                        $dateToString: {format: "%Y-%m-%d", date: "$createdAt"},
+                    },
+                    Total: {$sum: 1},
                 },
-                Total: { $sum: 1 },
             },
-        },
-        { $sort: { _id: 1 } },
-    ]);
+            {$sort: {_id: 1}},
+        ]);
 
-    const last5Followers = await Users.findById(userId).populate({
-        path: "followers",
-        options: { sort: { _id: -1 } },
-        perDocumentLimit: 5,
-        populate: {
-            path: "followerId",
-            select: "name email image accountType followers -password",
-        },
-    });
+        const followersStats = await Followers.aggregate([
+            {
+                $match: {
+                    writerId: new mongoose.Types.ObjectId(userId),
+                    createdAt: {$gte: startDate, $lte: currentDate},
+                },
+            },
+            {
+                $group: {
+                    _id: {
+                        $dateToString: {format: "%Y-%m-%d", date: "$createdAt"},
+                    },
+                    Total: {$sum: 1},
+                },
+            },
+            {$sort: {_id: 1}},
+        ]);
 
-    const last5Posts = await  Post.find({ user: userId })
+        const last5Followers = await Users.findById(userId).populate({
+            path: "followers",
+            options: {sort: {_id: -1}},
+            perDocumentLimit: 5,
+            populate: {
+                path: "followerId",
+                select: "name email image accountType followers -password",
+            },
+        });
 
-        .limit(5)
-        .sort({ _id: -1 });
+        const last5Posts = await Post.find({user: userId})
 
-    res.status(200).json({
-        success: true,
-        message: "Data loaded successfully",
-        totalPosts,
-        totalViews,
-        totalWriters,
-        followers: totalFollowers?.followers?.length,
-        viewStats,
-        followersStats,
-        last5Followers: last5Followers?.followers,
-        last5Posts,
-    });
-} catch (error) {
-    console.log(error);
-    res.status(404).json({ message: error.message });
-}
+            .limit(5)
+            .sort({_id: -1});
+
+        res.status(200).json({
+            success: true,
+            message: "Data loaded successfully",
+            totalPosts,
+            totalViews,
+            totalWriters,
+            followers: totalFollowers?.followers?.length,
+            viewStats,
+            followersStats,
+            last5Followers: last5Followers?.followers,
+            last5Posts,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({message: error.message});
+    }
 };
 
 export const getFollowers = async (req, res, next) => {
     try {
-        const { userId } =req.body.user;
+        const {userId} = req.body.user;
 
         // pagination
         const page = Number(req.query.page) || 1;
@@ -114,7 +114,7 @@ export const getFollowers = async (req, res, next) => {
 
         const result = await Users.findById(userId).populate({
             path: "followers",
-            options: { sort: { _id: -1 }, limit: limit, skip: skip },
+            options: {sort: {_id: -1}, limit: limit, skip: skip},
             populate: {
                 path: "followerId",
                 select: "name email image accountType followers -password",
@@ -134,26 +134,68 @@ export const getFollowers = async (req, res, next) => {
 
     } catch (error) {
         console.log(error);
-        res.status(404).json({ message: error.message });
+        res.status(404).json({message: error.message});
     }
 };
 
-export const getPostContent = async (req, res, next) => {}
+export const getPostContent = async (req, res, next) => {
+    try {
+        const {userId} = req.body.user;
 
-export const createPost = async (req, res, next) => {}
+        let queryResult = Posts.find({user: userId}).sort({
+            _id: -1,
+        });
 
-export const commentPost = async (req, res, next) => {}
+        // pagination
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 8;
+        const skip = (page - 1) * limit;
 
-export const updatePost = async (req, res, next) => {}
+        //records count
+        const totalPost = await Posts.countDocuments({ user: userId });
+        const numOfPage = Math.ceil(totalPost / limit);
 
-export const getPosts = async (req, res, next) => {}
+        queryResult = queryResult.skip(skip).limit(limit);
 
-export const getPopularContents = async (req, res, next) => {}
+        const posts = await queryResult;
 
-export const getPost = async (req, res, next) => {}
+        res.status(200).json({
+            success: true,
+            message: "Content Loaded successfully",
+            totalPost,
+            data: posts,
+            page,
+            numOfPage,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({message: error.message});
+    }
+};
 
-export const getComments = async (req, res, next) => {}
+export const createPost = async (req, res, next) => {
+}
 
-export const deletePost = async (req, res, next) => {}
+export const commentPost = async (req, res, next) => {
+}
 
-export const deleteComment = async (req, res, next) => {}
+export const updatePost = async (req, res, next) => {
+}
+
+export const getPosts = async (req, res, next) => {
+}
+
+export const getPopularContents = async (req, res, next) => {
+}
+
+export const getPost = async (req, res, next) => {
+}
+
+export const getComments = async (req, res, next) => {
+}
+
+export const deletePost = async (req, res, next) => {
+}
+
+export const deleteComment = async (req, res, next) => {
+}
