59c4d4318e382a438875e0f78b4a91dbe416db21
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-23 00:55:49 +0530

    getfollowers done

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -103,7 +103,40 @@ try {
 }
 };
 
-export const getFollowers = async (req, res, next) => {}
+export const getFollowers = async (req, res, next) => {
+    try {
+        const { userId } =req.body.user;
+
+        // pagination
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 8;
+        const skip = (page - 1) * limit; //2-1 * 8 = 8
+
+        const result = await Users.findById(userId).populate({
+            path: "followers",
+            options: { sort: { _id: -1 }, limit: limit, skip: skip },
+            populate: {
+                path: "followerId",
+                select: "name email image accountType followers -password",
+            },
+        });
+
+        const totalFollowers = await Users.findById(userId);
+
+        const numOfPages = Math.ceil(totalFollowers?.followers?.length / limit);
+
+        res.status(200).json({
+            data: result?.followers,
+            total: totalFollowers?.followers?.length,
+            numOfPages,
+            page,
+        });
+
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ message: error.message });
+    }
+};
 
 export const getPostContent = async (req, res, next) => {}
 
