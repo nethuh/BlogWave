f5fcf7e9ca4dedb1f74541a5f7d58610783bef0f
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-23 00:17:04 +0530

    updated

---
+++ server/controllers/postController.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import Posts from "../models/postModel.js";
+import Users from "../models/userModel.js";
+import Views from "../models/viewsModel.js";
+import Followers from "../models/followersModel.js";
+import Comments from "../models/commentModel.js";
+
+
+export const stats = async (req, res, next) => {
+
+}
+
+export const getFollowers = async (req, res, next) => {}
+
+export const getPostContent = async (req, res, next) => {}
+
+export const createPost = async (req, res, next) => {}
+
+export const commentPost = async (req, res, next) => {}
+
+export const updatePost = async (req, res, next) => {}
+
+export const getPosts = async (req, res, next) => {}
+
+export const getPopularContents = async (req, res, next) => {}
+
+export const getPost = async (req, res, next) => {}
+
+export const getComments = async (req, res, next) => {}
+
+export const deletePost = async (req, res, next) => {}
+
+export const deleteComment = async (req, res, next) => {}
