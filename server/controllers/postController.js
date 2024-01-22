abfdbfd11b69da66ffce664a8e5353d0acbb4f57
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-23 01:03:49 +0530

    updatePost done

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -239,7 +239,23 @@ export const commentPost = async (req, res, next) => {
 };
 
 export const updatePost = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { status } = req.body;
+
+        const post = await Posts.findByIdAndUpdate(id, { status }, { new: true });
+
+        res.status(200).json({
+            sucess: true,
+            message: "Action performed successfully",
+            data: post,
+        });
+    } catch (error) {
+    console.log(error);
+    res.status(404).json({ message: error.message });
 }
+};
+
 
 export const getPosts = async (req, res, next) => {
 }
