2ca294b0a86a4b8ddb6436365e57476e6f9e85e7
Sayani Nethmi <nethmihewage2002@gmail.com>
2024-01-22 20:58:12 +0530

    getWriter done

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -43,9 +43,9 @@ export const OPTVerification = async (req, res, next) => {
 
 export const resendOTP = async (req, res, next) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
 
-        await Verification.findOneAndDelete({userId: id});
+        await Verification.findOneAndDelete({ userId: id });
 
         const user = await Users.findById(id);
 
@@ -55,12 +55,13 @@ export const resendOTP = async (req, res, next) => {
 
         if (user?.accountType === "Writer") {
             sendVerificationEmail(user, res, token);
-        } else res.status(404).json({message: "Something went wrong"});
+        } else res.status(404).json({ message: "Something went wrong" });
     } catch (error) {
         console.log(error);
-        res.status(404).json({message: "Something went wrong"});
+        res.status(404).json({ message: "Something went wrong" });
     }
 };
+
 
 export const followWritter = async (req, res, next) => {
     try {
@@ -133,7 +134,26 @@ export const updateUser = async (req, res, next) => {
 
 export const getWriter = async (req, res, next) => {
     try {
+        const { id } = req.params;
 
+        const user = await Users.findById(id).populate({
+            path: "followers",
+            select: "followerId",
+        });
+
+        if (!user) {
+            return res.status(200).send({
+                success: false,
+                message: "Writer Not Found",
+            });
+        }
+
+        user.password = undefined;
+
+        res.status(200).json({
+            success: true,
+            data: user,
+        });
     } catch (error) {
         console.log(error);
         res.status(404).json({message: "Something went wrong"});
